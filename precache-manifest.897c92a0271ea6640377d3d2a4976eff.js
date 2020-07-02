c988857482e1f2530920ad3c98b22301157f82f0
Bartek Sieminski <b.r.sieminski@gmail.com>
2020-07-02 19:49:07 +0200

    Updates

---
+++ precache-manifest.897c92a0271ea6640377d3d2a4976eff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "a30e7edf8e0ef150cde0",
+    "url": "/app-rr/static/js/main.f1d9acf3.chunk.js"
+  },
+  {
+    "revision": "af2de68333ceb99c0516",
+    "url": "/app-rr/static/js/2.22b56b71.chunk.js"
+  },
+  {
+    "revision": "a30e7edf8e0ef150cde0",
+    "url": "/app-rr/static/css/main.ca9433a3.chunk.css"
+  },
+  {
+    "revision": "8cdf8c920691feb8d8386912d1ada9cf",
+    "url": "/app-rr/index.html"
+  }
+];
